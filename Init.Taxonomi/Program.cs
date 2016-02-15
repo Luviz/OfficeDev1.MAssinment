04e6e957dc1f519e90fcdb1584493ad6fecc9f8c
Bardia Jedi <bardiajedi@gmail.com>
2016-02-15 18:42:28 +0100

    Added GetPassword The GetPassword is a bash like PassGraber

--- Init.Taxonomi/Program.cs
+++ Init.Taxonomi/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,17 @@ namespace Init.Taxonomi {
 		static void Main(string[] args) {
 			Console.WriteLine("Doingsomething");
 			args.ToList().ForEach(Console.WriteLine);
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+
+
+		private static SecureString GetPassword() {
+			char charBuffer;
+			SecureString password = new SecureString();
+			while ((charBuffer = Console.ReadKey(true).KeyChar) != (char)ConsoleKey.Enter)
+				password.AppendChar(charBuffer);
+			return password;
 		}
 	}
 }
