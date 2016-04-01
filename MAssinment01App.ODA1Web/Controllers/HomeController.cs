b2853219f9fd31637058826b8d3f9550b9594cad
Bardia Jedi <bardiajedi@gmail.com>
2016-04-01 17:16:50 +0200

    Random Button Added ToDo Partial View for creating Orders

--- MAssinment01App.ODA1Web/Controllers/HomeController.cs
+++ MAssinment01App.ODA1Web/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MAssinment01App.ODA1Web.Models.Logic;
+using MAssinment01App.ODA1Web.Extations;
 
 namespace MAssinment01App.ODA1Web.Controllers {
 	public class HomeController : Controller {
@@ -22,6 +24,12 @@ namespace MAssinment01App.ODA1Web.Controllers {
 					clientContext.ExecuteQuery();
 					var customers = Models.Logic.Get_Customers.GetCustomers(clientContext);
 					var orders = Models.Logic.Get_Orders.GetOrders(clientContext);
+
+					customers.GetOrders(orders);
+
+					ViewBag.RandomCustomerWithNoOrders = customers.Where(c => !c.Orders.Any())
+						.ToList().GetRand();
+					ViewBag.RandomButtom = ViewBag.RandomCustomerWithNoOrders != null ? "" : "disabled=''";
 
 					ViewBag.NewstCustomers = customers.OrderByDescending(c => c.Created).Take(3) ;
 					ViewBag.NewstOrders = orders.OrderByDescending(o => o.Created).Take(5);
---
+++ MAssinment01App.ODA1Web/Extations/RandomFunThings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MAssinment01App.ODA1Web.Extations {
+	public static class RandomFunThings {
+		public static T GetRand<T>(this List<T> list) {
+			Random r = new Random();
+			return list.Any() ? list[r.Next(list.Count)] : default(T);
+		}
+	}
+}
